faf0f3ccfec6a1d98b532fee9a5d210110064fe3
SuperMan <80491636@qq.com>
2020-07-22 18:08:35 +0800

    抖音更新页面全部乱码。 不再从页面中找地址，改为获取302跳转页面，根据itemsid 获取直接获取json。

--- VideoEdit.py
+++ VideoEdit.py
@@ -4,17 +4,31 @@ import os
 import re
 import time
 import math
+import json
 from fake_useragent import UserAgent
 
 _urlList = []
 videoList = []
 
+# header = {
+#     'User-Agent': UserAgent("chrome").rget,
+#     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+#     "accept-encoding": "gzip, deflate, sdch, br",
+#     "accept-language": "en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4",
+#     "cache-control": "no-cache"
+# }
 header = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Connection': 'keep-alive',
+    'Host': 'v.douyin.com',
+    'Sec-Fetch-Dest': 'document',
+    "Sec-Fetch-Mode": 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Upgrade-Insecure-Requests': '1',
+    "cache-control": "no-cache",
     'User-Agent': UserAgent("chrome").rget,
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "accept-encoding": "gzip, deflate, sdch, br",
-    "accept-language": "en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4",
-    "cache-control": "no-cache"
 }
 
 
@@ -22,7 +36,7 @@ header = {
 class ReadTxt(object):
     def __init__(self):
         if (os.path.isfile("foo.txt")):
-            for line in open("foo.txt"):
+            for line in open("foo.txt", "r", encoding='utf-8'):
                 print(line)
                 _urlList.append(line.strip('\n'))
         else:
@@ -46,37 +60,46 @@ class ReadTxt(object):
     # 打开链接获取真实视频和图片地址
     def findViedoUrl(self, url):
         session = requests.Session()
-        res = session.get(url=url, timeout=5, headers=header)
+        res = session.get(url=url, timeout=5, headers=header, allow_redirects=False)
+
         print(res.status_code, " 打开状态 ", res.encoding)
         res.encoding = 'utf-8'
-        # print(res.text)
-        data = res.text
+        print(res.headers['Location'])
+        if (res.status_code == 302):
+            url = res.headers['Location']
+            items = url.split('/')
+            # print(items)
+        else:
+            print("网址打开错误：没有返回302正确地址：", res.status_code)
+        item_id = items[5]
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Host': 'www.iesdouyin.com',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 
-        searchObj = re.search('playAddr\: \"(.*)\"\,', data, re.M | re.I)
-        if searchObj:
-            # playAddr = searchObj.group(1).replace("/playwm/","/play/")
-            playAddr = searchObj.group(1)
-        else:
-            playAddr = ""
-        searchObj = re.search('cover\: \"(.*)\"', data, re.M | re.I)
-        if searchObj:
-            cover = searchObj.group(1)
-        else:
-            cover = ""
-        searchObj = re.search('\<p class=\"desc\"\>(.*?)</p>', data, re.M | re.I)
-        if searchObj:
-            filename = searchObj.group(1)
-        else:
-            filename = ""
-        if filename == "":
-            filename = math.floor(time.time())
-        print(
-            {
-                "playAddr": playAddr,
-                "cover": cover,
-                'filename': filename,
-            }
-        )
+        }
+        print(headers)
+        url = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=" + item_id
+        res = session.get(url=url)
+        res.encoding = 'utf-8'
+        print(type(res.text),type(res.content))
+        # n = json.dumps(res.text)
+        n = json.loads(res.text)
+        print(type(n))
+
+        playAddr = n['item_list'][0]['video']['play_addr']['url_list'][0]
+        cover = n['item_list'][0]['video']['origin_cover']['url_list'][0]
+        filename = n['item_list'][0]['share_info']['share_title']
+
         return {
                    "playAddr": playAddr,
                    "cover": cover,
@@ -98,7 +121,7 @@ class ReadTxt(object):
         try:
             videoBin = session.get(info['playAddr'], timeout=20, headers=head)
         except Exception as e:
-            print("url地址：",info['playAddr'],"\n","错误信息：",e)
+            print("url地址：", info['playAddr'], "\n", "错误信息：", e)
             return
         print(videoBin.status_code, " 视频状态 ")
         filename = info['filename']
@@ -111,7 +134,7 @@ class ReadTxt(object):
             os.mkdir('video')  # 创建目录
         with open('video/%s.mp4' % (mp4name), 'wb') as fb:  # 将下载的视频保存到对应的文件夹中
             fb.write(videoBin.content)
-            videoBin = session.get(info['cover'], timeout=5, headers=header)
+            videoBin = session.get(info['cover'], timeout=5)
             with open('video/%s.jpg' % (filename), 'wb') as fb:  # 将下载的图片保存到对应的文件夹中
                 fb.write(videoBin.content)
         time.sleep(2)
@@ -134,7 +157,9 @@ class Merge(object):
                     # 拼接成完整路径
                     filePath = os.path.join(root, file)
                     # 载入视频
+
                     video = VideoFileClip(filePath).resize((w, h))
+
                     # 添加到数组
                     L.append(video)
         print(L)
